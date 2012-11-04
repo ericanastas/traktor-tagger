804463864311bd846980d1c2d9e5e1b8a4ceca41
Eric Anastas <aireq303@gmail.com>
2012-11-04 13:52:35 +0000

    git-svn-id: https://traktor-tagger.googlecode.com/svn/trunk@4 5f656b78-51b1-4445-5cbd-0f0f318d7d98

---
+++ TracktorCollection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TracktorTagger
+{
+    public class TracktorCollection
+    {
+
+      
+           private IList<TracktorEntry> Entries {get; set;}
+
+           private System.Xml.XmlDocument _collectionXmlDoc;
+
+
+           public string FileName { get; set; }
+
+
+        public TracktorCollection(string fileName)
+        {
+            Entries = new List<TracktorEntry>();
+            FileName = fileName;
+
+            _collectionXmlDoc = new System.Xml.XmlDocument();
+            _collectionXmlDoc.Load(fileName);
+
+            var entryNodes = _collectionXmlDoc.DocumentElement.SelectNodes("/NML/COLLECTION/ENTRY");
+
+            foreach(System.Xml.XmlElement entryNode in entryNodes)
+            {
+                Entries.Add(new TracktorEntry(entryNode));
+            }
+
+           
+        }
+
+    }
+}
---
+++ TracktorEntry.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TracktorTagger
+{
+
+    public class Location
+    {
+
+        public string Directory { get; set; }
+        public string FileName { get; set; }
+        public string Volume { get; set; }
+        public string VolumeId { get; set; }
+
+    }
+
+    class TracktorEntry
+    {
+        private System.Xml.XmlElement entryNode;
+
+        public TracktorEntry(System.Xml.XmlElement entryNode)
+        {
+            this.entryNode = entryNode;
+        }
+
+
+        public string Title 
+        {
+            get
+            {
+                return entryNode.Attributes["TITLE"].Value;
+            }
+            set
+            {
+                entryNode.Attributes["TITLE"].Value = value;
+            }
+        }
+
+
+
+        public string Artist
+        {
+            get
+            {
+                return entryNode.Attributes["ARTIST"].Value;
+            }
+            set
+            {
+                entryNode.Attributes["ARTIST"].Value = value;
+            }
+        }
+
+
+        public string Mix { get; set; }
+        public string Remixer { get; set; }
+        public string AlbumTitle { get; set; }
+
+        public string Key { get; set; }
+        public double BPM { get; set; }
+        public string Lyrics { get; set; }
+
+        public string Label { get; set; }
+        public string CatalogNumber { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Comment1 { get; set; }
+        public string Comment2 { get; set; }
+
+
+        public DateTime ModifiedDate { get; set; }
+        public DateTime ImportDate { get; set; }
+
+
+        public int FileSize { get; set; }
+        public int BitRate { get; set; }
+        public Location Location { get; set; }        
+
+        public int Ranking { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public int PlayTime { get; set; }
+        public double PlayTimeFloat { get; set; }
+
+
+        public override string ToString()
+        {
+            return this.Artist + " " + this.Title + " " + this.Mix + " " + this.Label;
+        }
+
+
+
+    }
+}
+
+
