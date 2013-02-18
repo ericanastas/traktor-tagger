5220383df0e1695e62077d9e47e096aaa015e21e
Eric Anastas <aireq303@gmail.com>
2013-02-18 03:41:24 +0000

    git-svn-id: https://traktor-tagger.googlecode.com/svn/trunk@100 5f656b78-51b1-4445-5cbd-0f0f318d7d98

---
+++ TraktorTagger/DateConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TraktorTagger
+{
+    [ValueConversion(typeof(DateTime?), typeof(string))]
+    class DateConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            DateTime? dt = (DateTime?)value;
+            string format= (string)parameter;
+
+            if(dt.HasValue) return dt.Value.ToString(format);
+            else return null;
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string dateStr = (string)value;
+
+            if(String.IsNullOrEmpty(dateStr)) return null;
+            else return DateTime.Parse(dateStr);
+        }
+    }
+}
